e794e7781f8b7e572f188afb803f35cc6bcb3347
Takuya Yonezawa <yonezawa.takuya.yq2@is.naist.jp>
2021-05-18 23:52:45 +0900

    up

--- test-python/build/app/api.py
+++ test-python/build/app/api.py
@@ -8,35 +8,38 @@ app = FastAPI()
 
 @app.get('/') 
 async def hello(response: Response):
+    return {"text": "root!"}
+@app.get('/api/') 
+async def hello(response: Response):
     return {"text": "helleo world!"}
 
-@app.get('/set_cookie') 
+@app.get('/api/set_cookie') 
 async def set_cookie(response: Response):
     # define value
     user='user01'
     unixtime = datetime.datetime.now().strftime('%s')
     cookie = jwt.encode({'user':user ,'unixtime':unixtime}, 'secret', algorithm='HS256')
     # set cookie to redis
-    redis_cli = redis.Redis(host='redis-service', port=6379, db=0)
-    redis_cli.set(user, cookie)
+    # redis_cli = redis.Redis(host='redis-service', port=6379, db=0)
+    # redis_cli.set(user, cookie)
     # set cookie to response
     response.set_cookie('mycookie', cookie)
     return {"cookie setted!": cookie}
 
-@app.get('/get_cookie') 
+@app.get('/api/get_cookie') 
 async def get_cookie(response: Response):
     # define value
     user='user01'
     # get cookie from redis
-    redis_cli = redis.Redis(host='redis-service', port=6379, db=0)
-    cookie = redis_cli.get(user)
+    # redis_cli = redis.Redis(host='redis-service', port=6379, db=0)
+    # cookie = redis_cli.get(user)
     # return cookie from redis
     return {"cookie getted!":cookie.decode()}
 
-@app.get('/json') 
+@app.get('/api/json') 
 async def hello():
     return {"text": "Json!"}
 
-@app.get('/api') 
+@app.get('/api/api') 
 async def hello():
     return {"text": "APIAPIAPI!"}
